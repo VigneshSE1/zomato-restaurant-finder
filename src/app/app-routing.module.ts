6c0bd87ddcdcd95c310bdbcf58970f950366ae32
VigneshSE1 <48944738+VigneshSE1@users.noreply.github.com>
2020-02-21 02:56:09 +0530

    zomato restaurant finder complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HotelcardComponent } from './hotelcard/hotelcard.component';
+
 
 const routes: Routes = [];
 
---
+++ src/app/hotelcard/hotelcard.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from "@angular/forms";
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+@Component({
+  selector: 'app-hotelcard',
+  templateUrl: './hotelcard.component.html',
+  styleUrls: ['./hotelcard.component.css']
+})
+export class HotelcardComponent implements OnInit {
+
+  constructor(private http: HttpClient, ) { }
+
+  formData = new FormGroup({
+    keyword: new FormControl(''),
+    sort: new FormControl(''),
+    order: new FormControl(''),
+  });
+
+  urlParameters;
+  setParams(startValue) {
+    let params = new HttpParams()
+      .set("sort", this.formData.value["sort"])
+      .set("order", this.formData.value["order"])
+      .set("q", this.formData.value["keyword"])
+      .set("start", startValue)
+    console.log(this.formData.value);
+    console.log(params.toString());
+    this.urlParameters = params.toString();
+
+    this.getdata(this.urlParameters);
+  }
+
+  ngOnInit() {
+    this.getdata(this.urlParameters);
+  }
+  restaurantArray;
+
+
+  getdata(urlQuery) {
+    this.http.get('https://developers.zomato.com/api/v2.1/search?entity_id=7&entity_type=city&count=100&' + urlQuery, {
+      headers: { 'Content-Type': 'application/json', 'user-key': 'f985d357708f9e0123d0ab364d60a499' },
+    })
+      .subscribe((data) => {
+        this.restaurantArray = data["restaurants"]
+        // console.log(this.restaurantArray);
+        // console.log(this.restaurantArray[0]["restaurant"]["name"])
+        window.scroll(0, 0);
+      }, (err) => {
+        console.log(err);
+      })
+
+  }
+
+}
